d1a826558e8180c26794ef5ce3c07a7c2714f0f5
Menachem Geva <me.geva@gmail.com>
2021-07-05 14:27:20 +0300

    Update covert_xml_to_df.py

--- util/covert_xml_to_df.py
+++ util/covert_xml_to_df.py
@@ -1,5 +1,5 @@
 import xmltodict
-with open(“Quote-Order Int.XML”) as xml_file
+with open('Quote-Order Int.XML') as xml_file
     data_dict = xmltodict.parse(xml_file.read())
 #assuming there are redundent hirarchy of INPUT leve 
 df = pd.DataFrame.from_dict(data_dict[‘INPUT’][‘ITEMS’])
